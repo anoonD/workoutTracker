1812b90682cfed55e5dff6754288f6a05dda8497
you@example.co <you@example.co>
2022-09-20 21:45:20 +0000

    poop

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <string>
-#include <sqlite3.h>
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
-int main(int argc, char** argv)
-{
-    sqlite3 x();
-    sqlite3_open("", );
+int main() {
 }
